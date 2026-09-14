d1c44eb8e920df29e645151a8c054ecdf1b2e2fa
safarisoftwares-code <safarisoftwares@gmail.com>
2026-09-14 19:27:09 +0300

    Switch from Formspree to FormSubmit

--- js/script.js
+++ js/script.js
@@ -72,7 +72,7 @@ contactForm.addEventListener('submit', async (e) => {
     const data = Object.fromEntries(formData.entries());
     
     // Replace with your Formspree endpoint
-    const FORM_ENDPOINT = 'https://formspree.io/f/mljeqlnz';
+    const FORM_ENDPOINT = 'https://formsubmit.co/info@safarisoftwares.co.ke';
     
     try {
         const response = await fetch(FORM_ENDPOINT, {
