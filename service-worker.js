75052666ffaa498113647a0d2bbd9b3616fc13b6
safarisoftwares-code <safarisoftwares@gmail.com>
2026-09-26 03:50:18 +0300

    Add hard refresh button + auto-updating service worker v2

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,17 @@
-﻿// Safari Softwares Service Worker
-const CACHE_NAME = 'safari-softwares-v1';
+﻿const CACHE_VERSION = 'safari-softwares-v2.0';
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/assets/icon.svg'
+  '/assets/icon.svg',
+  '/assets/lion-header.png',
+  '/assets/icon-192.png',
+  '/assets/icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches.open(CACHE_VERSION)
       .then((cache) => cache.addAll(urlsToCache))
       .then(() => self.skipWaiting())
   );
@@ -20,7 +22,7 @@ self.addEventListener('activate', (event) => {
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
+          if (cacheName !== CACHE_VERSION) {
             return caches.delete(cacheName);
           }
         })
@@ -33,16 +35,31 @@ self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
   if (!event.request.url.startsWith(self.location.origin)) return;
 
+  const url = new URL(event.request.url);
+
+  if (url.pathname === '/' || url.pathname.endsWith('.html')) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const responseClone = response.clone();
+          caches.open(CACHE_VERSION).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request).then((fetchResponse) => {
-        return caches.open(CACHE_NAME).then((cache) => {
+        return caches.open(CACHE_VERSION).then((cache) => {
           cache.put(event.request, fetchResponse.clone());
           return fetchResponse;
         });
       });
-    }).catch(() => {
-      return caches.match('/index.html');
-    })
+    }).catch(() => caches.match('/index.html'))
   );
 });
